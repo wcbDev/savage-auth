8f4bd15841c032d36b8e56dcf7f8d8864e22991e
William Bell <113270654+wcbDev@users.noreply.github.com>
2022-11-06 18:43:10 -0500

    updated database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-    'dbName': 'demo'
+    'url' : 'mongodb+srv://bellw10:freshman11@cluster0.rp9us5a.mongodb.net/Savage_auth?retryWrites=true&ssl=true&authSource=admin', 
+    'dbName': 'Savage_auth'
 };
